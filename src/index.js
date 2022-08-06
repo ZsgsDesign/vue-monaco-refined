f4902d2a69cbfb49120c3639b4f780743280e085
zsgsdesign <zsgsdesign@gmail.com>
2022-08-07 00:27:43 +0800

    lint

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import MonacoEditor from './MonacoEditor'
 export default MonacoEditor
 
 if (typeof window !== 'undefined' && window.Vue) {
-  window.Vue.component(MonacoEditor.name, MonacoEditor)
+    window.Vue.component(MonacoEditor.name, MonacoEditor)
 }
